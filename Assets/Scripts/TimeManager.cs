0f80a58d810d46ee8fc473700f0bcf73ca00a194
RafliIhsanuddin <93479421+RafliIhsanuddin@users.noreply.github.com>
2024-06-19 09:47:56 +0700

    feat : Timer Implemented

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TimeManager : MonoBehaviour
 {
+
+
+    public float TimeRemaining;
+
+    public TextMeshProUGUI timeText;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,25 @@ public class TimeManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if(TimeRemaining >= 0)
+        {
+            TimeRemaining -= Time.deltaTime;
+
+            
+
+            timeText.text = "Time : " + TimeRemaining.ToString();
+        }
+        else
+        {
+            if(TimeRemaining < 0)
+            {
+                TimeRemaining = 0;
+            }
+            timeText.text = "Time : " + TimeRemaining.ToString();
+        }
+
+
+
     }
 }
